b8e740a1b180bfa99a1774103d99162095da7b37
Alexander Sladkikh <sas.gta@ya.ru>
2024-05-17 13:27:33 +0500

    finish marketing page

--- app/(marketing)/_components/heroes.tsx
+++ app/(marketing)/_components/heroes.tsx
@@ -5,10 +5,16 @@ export const Heroes = () => {
         <div className="flex flex-col items-center justify-center max-w-5xl">
             <div className="flex items-center">
                 <div className="relative h-[300px] w-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
-                    <Image 
+                    <Image
                         src="/documents.png"
                         fill
-                        className="object-contain"
+                        className="object-contain dark:hidden"
+                        alt="Documents"
+                    />
+                    <Image
+                        src="/documents-dark.png"
+                        fill
+                        className="object-contain hidden dark:block"
                         alt="Documents"
                     />
                 </div>
@@ -16,7 +22,13 @@ export const Heroes = () => {
                     <Image
                         src="/reading.png"
                         fill
-                        className="object-contain"
+                        className="object-contain dark:hidden"
+                        alt="Reading"
+                    />
+                    <Image
+                        src="/reading-dark.png"
+                        fill
+                        className="object-contain hiddendark:block"
                         alt="Reading"
                     />
                 </div>
