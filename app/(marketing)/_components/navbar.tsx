d36952edae33f1ca864204e230a72cb1550a8b74
Alexander Sladkikh <sas.gta@ya.ru>
2024-05-19 13:09:04 +0500

    add login logic and buttons

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 "use client";
 
+import { useConvexAuth } from "convex/react";
+import { SignIn, SignInButton, UserButton } from "@clerk/clerk-react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Spinner } from "@/components/spinner";
+import Link from "next/link"
 
 export const Navbar = () => {
+    const {isAuthenticated, isLoading} = useConvexAuth();
     const scrolled = useScrollTop();
 
     return (
@@ -14,6 +20,35 @@ export const Navbar = () => {
         )}>
             <Logo/>
             <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+                {isLoading && (
+                    <Spinner/>
+                )}
+                {isAuthenticated && !isLoading && (
+                    <>
+                        <div className="flex gap-x-1">
+                            <Button variant="outline" size="sm" asChild>
+                                <Link href="/documents">See my documents</Link>
+                            </Button>
+                            <UserButton
+                                afterSignOutUrl="/"
+                            />
+                        </div>
+                    </>
+                )}
+                {!isAuthenticated && !isLoading && (
+                    <>
+                        <SignInButton mode="modal">
+                            <Button variant="outline" size="sm">
+                                Log in
+                            </Button>
+                        </SignInButton>
+                        <SignInButton mode="modal">
+                            <Button size="sm">
+                                Get jotion for free
+                            </Button>
+                        </SignInButton>
+                    </>
+                )}
                 <ModeToggle/>
             </div>
         </div>
---
+++ convex/auth.config.js
@@ -0,0 +1,9 @@
+/* trunk-ignore-all(eslint/import/no-anonymous-default-export) */
+export default {
+  providers: [
+    {
+      domain: "https://smooth-ladybird-38.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
